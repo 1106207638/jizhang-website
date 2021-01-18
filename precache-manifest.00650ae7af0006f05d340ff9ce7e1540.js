fe8fba92726695b04831d5102453b817057e2f83
1106207638 <1106207638@qq.com>
2021-01-18 23:09:20 +0800

    deploy

---
+++ precache-manifest.00650ae7af0006f05d340ff9ce7e1540.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde23d946b7a23097f0b",
+    "url": "/jizhang-website/css/app.f206a4c7.css"
+  },
+  {
+    "revision": "e8cb216618d7d28734a72b982a053c37",
+    "url": "/jizhang-website/index.html"
+  },
+  {
+    "revision": "dde23d946b7a23097f0b",
+    "url": "/jizhang-website/js/app.2671b920.js"
+  },
+  {
+    "revision": "0173f534991640b6750c",
+    "url": "/jizhang-website/js/chunk-vendors.3f274a7c.js"
+  },
+  {
+    "revision": "6a6e5c07675a568d1d24a3c5b129de92",
+    "url": "/jizhang-website/manifest.json"
+  },
+  {
+    "revision": "b28bdf9cec8550f57a3c46375bbcb20c",
+    "url": "/jizhang-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jizhang-website/robots.txt"
+  }
+]);
